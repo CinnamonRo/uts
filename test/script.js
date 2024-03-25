cc3a8d5fb0f0a444f8b4cdcd1a3b2ac7a6e71df2
Cinamon <huangfredlie04@gmail.com>
2024-03-25 09:58:22 +0700

    test

--- test/script.js
+++ test/script.js
@@ -4,10 +4,13 @@ const nameShow = document.getElementById("nameShow");
 
 nameSubmit.addEventListener("click", function () {
   const username = nameInput.value;
+  console.log(username);
+  //   alert("hai");
   if (nameInput.trim() !== "") {
     nameShow.textContent = username;
+    tampilkan.classList.remove("d-none");
+    indexbody.classList.add("d-none");
   } else {
     alert("Please enter your name");
   }
-  console.log("hai");
 });
