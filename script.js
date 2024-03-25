dfcd17f35c9bcf12603fdfe6fc8e8ed6d2304c1a
Cinamon <huangfredlie04@gmail.com>
2024-03-25 16:13:22 +0700

    a

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 // index
 $(document).ready(function () {
+  //login
   $("#login").submit(function (event) {
     event.preventDefault(); // prevent form submit
-    var username = "Hallo " + $("#inputName").val();
-    localStorage.setItem("username", username);
-    window.location.href = "main.html";
+    if ($("#inputName").val() !== "") {
+      localStorage.setItem("username", username);
+      window.location.href = "main.html";
+    } else {
+      alert("masukkan namamu");
+    }
   });
-});
 
-//header
-$(document).ready(function () {
+  //header
   var username = localStorage.getItem("username");
   $("#nameShow").text(username);
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-  let localStorageKey = "";
-  if (window.location.href.includes("income.html")) {
-    localStorageKey = "incomeExpenses";
-  } else if (window.location.href.includes("outcome.html")) {
-    localStorageKey = "outcomeExpenses";
-  }
+  //outcome
 });
